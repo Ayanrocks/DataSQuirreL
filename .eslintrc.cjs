e080a520262586a6e69286a66a4d46879ed19472
Ayan Banerjee <ayanrocks@Ayans-MacBook-Pro.local>
2025-05-04 11:32:39 +0530

    Added support for eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    extraFileExtensions: ['.svelte'],
+  },
+  settings: {
+    'svelte/preprocess': true,
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
+  rules: {
+    // Add any specific rules or overrides here
+  },
+};
